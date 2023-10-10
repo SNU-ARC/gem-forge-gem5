29854c6e053461766ed444cd2e704e31f0b46213
k16diablo <miguel92@naver.com>
2023-10-10 23:35:14 +0900

    typo fixed

--- src/arch/x86/isa/insts/psp/psp.py
+++ src/arch/x86/isa/insts/psp/psp.py
@@ -2,32 +2,32 @@
 microcode = '''
 def macroop STREAM_CFG_IDX_BASE
 {
-    stream_cfg_idx_base reg imm
+    stream_cfg_idx_base reg, imm
 };
 
 def macroop STREAM_CFG_IDX_GRAN
 {
-    stream_cfg_idx_gran reg imm
+    stream_cfg_idx_gran reg, imm
 };
 
 def macroop STREAM_CFG_VAL_BASE
 {
-    stream_cfg_val_base reg imm
+    stream_cfg_val_base reg, imm
 };
 
 def macroop STREAM_CFG_VAL_GRAN
 {
-    stream_cfg_val_gran reg imm
+    stream_cfg_val_gran reg, imm
 };
 
 def macroop STREAM_INPUT_OFFSET_BEGIN
 {
-    stream_input_offset_begin reg imm
+    stream_input_offset_begin reg, imm
 };
 
 def macroop STREAM_INPUT_OFFSET_END
 {
-    stream_input_offset_end reg imm
+    stream_input_offset_end reg, imm
 };
 
 def macroop STREAM_TERMINATE
